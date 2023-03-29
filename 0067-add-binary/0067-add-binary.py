88b70f7db7214078d3ccf3d712f502549975baa4
sigma-wbi <81278907+sigma-wbi@users.noreply.github.com>
2023-03-29 17:25:40 +0900

    Time: 26 ms (96.60%), Space: 14 MB (18.10%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -2,6 +2,8 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         
         res10 = int(a,2) + int(b,2)
+        
+        # 'b' : 2진수로 변환 'o' : 8진수로 변환 'x' : 16진수로 변환
         res2 = format(res10,'b')
         
         return res2
