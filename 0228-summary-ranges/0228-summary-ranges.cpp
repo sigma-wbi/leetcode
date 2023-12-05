c88e069d0d5649ed6ffd314e017741757150d22f
WooBI <drwoo0824@naver.com>
2023-12-05 23:00:12 +0900

    Time: 0 ms (100.00%), Space: 7.1 MB (92.44%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string makeRange(int start, int end) {
+        ostringstream oss;
+        if (start != end) {
+            oss << start << "->" << end;
+        } else {
+            oss << start;
+        }
+        return oss.str();
+    }
+    
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> result;
+        int len = nums.size();
+        if (len == 0) return result;
+        
+        int start=nums[0], end=nums[0];
+
+        for (int i=1; i<len; i++) {
+            if (nums[i] == end + 1) {
+                end = nums[i];
+                continue;
+            }
+            
+            result.push_back(makeRange(start, end));
+            start = end = nums[i];
+        
+        }
+        
+        result.push_back(makeRange(start, end)); 
+        
+        return result;
+    }
+};
